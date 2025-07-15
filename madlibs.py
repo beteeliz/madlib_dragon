c308a3ca09c10740e3f707506b17a7577faae01b
Elizabete Nakamura <44741912+beteeliz@users.noreply.github.com>
2025-07-15 12:17:30 +0200

    Add files via upload
    Madlib Python Project

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+dragon_color = input("Dragon color: ")
+dragon_personality = input("Dragon personality: ")
+hobby = input("Hobby: ")
+visitor = input("Visitor: ")
+
+story = f"""
+Once upon a time, there was a {dragon_color} dragon who lived on top of a high mountain.
+The dragon was not fierce, but very {dragon_personality} and loved to {hobby} every evening.
+One day, a {visitor} climbed the mountain and became the dragon's first and best friend.
+"""
+
+print(story)
